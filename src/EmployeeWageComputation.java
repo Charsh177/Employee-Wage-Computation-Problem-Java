68f74b46d228e59d2e991c7e6c0c5c8f384ee005
Charsh177 <harshchougule177@gmail.com>
2022-05-26 19:52:12 +0530

    ADDED: Compute employee wage for multiple companies

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,24 +1,24 @@
 public class EmployeeWageComputation
 {
-    public static void calculateTotalWage()
-    {
+    public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingHrs, int maxWorkingDays) {
         final int IS_PART_TIME = 1;
-        final int IS_FULL_TIME = 2;
-        final int WAGE_PER_HR = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
-
+        final int IS_Full_TIME = 2;
         int totalWage = 0;
         int workingHrs = 0;
-        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
-        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
-        {
 
-            int empType = (int) (Math.random() * 100) % 3;
-            switch (empType)
+        System.out.println("Details of " + companyName + " employee");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Wage per hour: " + wagePerHr);
+        System.out.println("Maximun working hours: " + maxWorkingHrs);
+        System.out.println("Maximum working days: " + maxWorkingDays);
+        System.out.printf("%5s     %5s     %5s     %5s     \n", "Day", "Workinghrs", "Wage", "Total working hrs");
+
+        for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays && totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHrs)
+        {
+            int empCheck = (int) (Math.random()*100 % 3);
+            switch (empCheck)
             {
-                case IS_FULL_TIME:
+                case IS_Full_TIME:
                     workingHrs = 8;
                     break;
                 case IS_PART_TIME:
@@ -28,16 +28,15 @@ public class EmployeeWageComputation
                     workingHrs = 0;
                     break;
             }
-            int wage = workingHrs * WAGE_PER_HR;
+            int wage = workingHrs * wagePerHr;
             totalWage += wage;
-            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
-
+            System.out.printf("%5d     %5d     %5d     %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
-        System.out.println("Total wage for a month is " + totalWage);
+        System.out.println("Total wage for a month of "+ companyName + " employee is " + totalWage + "\n");
     }
-
     public static void main(String[] args)
     {
-        calculateTotalWage();
+        calculateTotalWage("Facebook", 50, 15, 250);
+        calculateTotalWage("Big Bazaar", 30, 20, 150 );
     }
 }
